7428fc44ab1170a487252f92379a3233d2f486af
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2026-08-12 21:53:06 +0530

    fix: docker compose port settings

--- api/main.py
+++ api/main.py
@@ -93,4 +93,4 @@ async def explain_route(input_data: ApplicantInput) -> ExplainResponse:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("api.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="127.0.0.1", port=8100, reload=True)
